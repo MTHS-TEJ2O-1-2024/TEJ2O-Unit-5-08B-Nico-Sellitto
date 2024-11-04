2bd3580520982e1b0b4ab0a01a22075594b3ce34
Nico-Sellitto <nico.sellitto@mths.ca>
2024-11-04 11:10:23 -0500

    code

--- main.ts
+++ main.ts
@@ -5,3 +5,27 @@
  * This program make a car move and turn away from an object
 */
 
+// setup
+let distance: number = 0
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+while (true) {
+    distance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    // drives the car if its over 10 cm away from something
+    while (distance > 10) {
+        distance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    // turns car if its 10 cm away from something
+    robotbit.StpCarMove(1, 48)
+    } if (distance < 10) {
+        robotbit.StpCarTurn(90, 48, 125)
+    }
+}  
